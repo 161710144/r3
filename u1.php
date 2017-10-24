034eef701912ba33e186ec1016211f5a3b53329f
unknown <161710116@smkassalaambandung.sch.id>
2017-10-24 17:36:49 +0700

    latihan 3

---
+++ u1.php
@@ -0,0 +1,14 @@
+<?php
+class pokok{
+public $mie;
+public $beras;
+public $minyak;
+
+
+public function __construct($mie,$beras,$minyak){
+$this->mie = $mie;
+$this->beras = $beras;
+$this->minyak = $minyak;
+}
+}
+?>
---
+++ u2.php
@@ -0,0 +1,11 @@
+<center>
+<?php
+require_once'u1.php';
+
+$p1 = new pokok("Rp200.000","Rp300.000","Rp60.000");
+echo "Harga Mie instan adalah ".$p1->mie.'<br>';
+echo "Harga Beras adalah ".$p1->beras.'<br>';
+echo "Harga Minyak adalah ".$p1->minyak.'<br>';
+//echo "Melahirkan di ".$bio1->tempat_lahir.'<br>';
+?>
+</center>
